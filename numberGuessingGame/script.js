d575d47b269d1c4e9caa271cec0797a0a202f0d5
Cherrise Hatcher <cherrisehatcher@gmail.com>
2021-01-07 17:58:41 -0500

    fixed bugs

--- numberGuessingGame/script.js
+++ numberGuessingGame/script.js
@@ -3,13 +3,14 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
+
 const displayMessage = function (message){
     document.querySelector('.message').textContent = message;
     };
 
     document.querySelector('.check').addEventListener('click',function(){
-        const guess = Number(document.querySelector('.guess').nodeValue);
-        console.log(guess,typeof guess);
+        const guess = Number(document.querySelector('.guess').value);
+        console.log(guess, typeof guess);
   
     //When there is no input
     if(!guess){
